e5a2e82114aab73a6491ea2c9094b37c5780f8b0
FoxLib <hplus.foxtail@gmail.com>
2020-07-07 19:34:32 +0300

    Разделение на библиотеки

--- easycpu/app/01_hello/make.bat
+++ easycpu/app/01_hello/make.bat
@@ -1,6 +1,6 @@
 @echo off
 
-php ../../asm/easy2fasm.php hello.asm > hello.s
+php ../../asm/easy2fasm.php hello.asm inc=../../inc > hello.s
 fasm hello.s
 php ../../asm/easy2mif.php hello.bin > ../../mc3/prgram.mif
 cd ../../mc3
