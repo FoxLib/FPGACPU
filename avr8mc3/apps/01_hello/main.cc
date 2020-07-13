c6b799b73c14707be8f38a5d68c15843b1b347bc
FoxLib <hplus.foxtail@gmail.com>
2020-07-13 10:03:25 +0300

    Добавил процедуру печати символов

--- avr8mc3/apps/01_hello/main.cc
+++ avr8mc3/apps/01_hello/main.cc
@@ -6,10 +6,7 @@ int main() {
 
     screen scr;
 
-    //scr.print("test");
-    for (int i = 0; i < 200; i++)
-    for (int j = 0; j < 320; j++)
-        scr.pset(j, i, i + j);
+    scr.print("Hello, World!");
 
     for(;;);
 }
--- avr8mc3/include/screen.cc
+++ avr8mc3/include/screen.cc
@@ -4,9 +4,17 @@ class screen {
 protected:
 
     word cursor_x, cursor_y;
+    byte color_fr;
 
 public:
-/*
+
+    screen() {
+
+        cursor_x = 0;
+        cursor_y = 0;
+        color_fr = 15;
+    }
+
     // Печать символа 4x8 на экране. Допустиы только 0x20 - 0x7E
     void print4(char ch) {
 
@@ -14,32 +22,33 @@ public:
             return;
 
         ch -= 0x20;
+        word id = (ch&0x7E)<<2;
+        ch &= 1;
+
         for (int i = 0; i < 8; i++) {
 
-            char cb = ansi3[ch >> 1][i];
-                 cb = ch & 1 ? cb & 15 : cb >> 4;
+            char cb = pgm_read_byte(&ansi3[id + i]);
+                 cb = ch? cb & 15 : cb >> 4;
 
             for (int j = 0; j < 4; j++) {
                 if (cb & (1 << (3 - j)))
-                    pset(g_cursor_x + j, g_cursor_y + i, g_cursor_cl);
+                    pset(cursor_x + j, cursor_y + i, color_fr);
             }
         }
 
-        g_cursor_x += 4;
+        cursor_x += 4;
 
         // Перемотка
-        if (g_cursor_x >= 320) {
+        if (cursor_x >= 320) {
 
-            g_cursor_x = 0;
-            g_cursor_y++;
+            cursor_x = 0;
+            cursor_y += 8;
 
-            // Нужна ли тут перемотка экрана?
-            if (g_cursor_y >= 200) {
-                g_cursor_y = 192;
+            if (cursor_y >= 200) {
+                cursor_y = 192;
             }
         }
     }
-*/
 
     // Рисование точки на экране
     void pset(int x, int y, char cl) {
@@ -55,10 +64,13 @@ public:
         vm[z] = x & 1 ? ((vm[z] & 0xF0) | cl) : ((vm[z] & 0x0F) | (cl << 4));
     }
 
+    // Печать строки
     void print(const char* s) {
 
-        display(vm);
+        int i = 0;
 
-
+        while (byte b = s[i]) {
+            print4(b); i++;
+        }
     }
 };
