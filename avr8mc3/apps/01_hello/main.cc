360efff6c0262b84828bdacafd2d3b81dde89153
FoxLib <hplus.foxtail@gmail.com>
2020-07-13 14:46:57 +0300

    ... какой-то мелкий фикс ...

--- avr8mc3/apps/01_hello/main.cc
+++ avr8mc3/apps/01_hello/main.cc
@@ -8,20 +8,20 @@ stdio io;
 int main() {
 
     D.cls(1);
-    D.circlef(160, 100, 20, 14);
+    D.circlef(160, 100, 50, 14);
 
     D.color(15, 1);
     D.locate(8, 8);
-    D.print("Hello, World!");
+    D.print("Hello, World! Support only English for compact progmem size... ");
 
     D.lineb(1,1,318,198,7);
     D.lineb(3,3,316,196,7);
 
     for(;;) {
 
-        //D.locate(8, 16);
-        //D.print(io.timer());
+        D.locate(8, 16);
+        D.print(io.timer());
 
-        D.print4( io.getch() );
+        //D.print4( io.getch() );
     }
 }
