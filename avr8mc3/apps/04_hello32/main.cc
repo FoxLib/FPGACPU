6a35ad8242eead44fff44786664907f0a66855d2
FoxLib <hplus.foxtail@gmail.com>
2020-07-16 11:33:47 +0300

    Moar

--- avr8mc3/apps/04_hello32/main.cc
+++ avr8mc3/apps/04_hello32/main.cc
@@ -2,15 +2,20 @@
 
 graphics D;
 
-// Шаблон с чтением из PGM
 int main() {
 
     D.cls(0);
 
-    D.circlef(255, 120, 64, 1);
+    D.circlef(255, 120, 128, 1);
     D.locate(8, 8);
     D.print("Hello world! This 256x192 pix, 6kb. 32kb FLASH ROM. 8kb RAM");
 
+    D.block(10, 30, 160, 100, 1);
+    D.block(11, 31, 159, 39, 0);
+    D.locate(12, 32); D.print("Windows XP");
+    D.color(0, 1);
+    D.locate(12, 42); D.print("Yeto cherno-beloe izobrazhenie desu");
+    D.lineb(9, 29, 161, 101, 0);
 
     // Бесконечный цикл
     for(;;);
