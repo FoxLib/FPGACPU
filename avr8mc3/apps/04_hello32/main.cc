d90c142b9875d704ae4bed2cd41f4b71fec72985
FoxLib <hplus.foxtail@gmail.com>
2020-07-16 10:43:02 +0300

    Доработка вывода block

--- avr8mc3/apps/04_hello32/main.cc
+++ avr8mc3/apps/04_hello32/main.cc
@@ -6,10 +6,18 @@ graphics D;
 int main() {
 
     D.cls(1);
+
     D.line(0, 0, 255, 191, 0);
-    D.circle(128, 96, 16, 0);
+    D.circlef(128, 96, 16, 0);
     D.locate(8, 8);
     D.print("Hello world!");
+
+    D.block(7, 9, 15, 9, 0);
+    D.block(8, 10, 16, 10, 0);
+
+    D.block(0, 150, 255, 191, 0);
+    D.block(0, 155, 127, 160, 1);
+
 
     // Бесконечный цикл
     for(;;);
