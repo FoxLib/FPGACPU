b644bf8c0017bc8fab95dc4b5f8163129ef57875
kotolisic <kotolisic@gmail.com>
2020-08-12 09:02:14 +0300

    Первый код

--- 6502/de0/cpu.v
+++ 6502/de0/cpu.v
@@ -7,4 +7,26 @@ module cpu(
     output  reg         wren
 );
 
+`include "cpu_decl.v"
+
+always @(*) begin
+
+    alu_res  = 8'h00;
+    alu_flag = P;
+
+    case (alu)
+
+        alu_ora: alu_res = A | src;
+        alu_and: alu_res = A & src;
+        alu_eor: alu_res = A ^ src;
+        alu_adc: alu_res = A + src + P[flag_carry];
+        alu_sta,
+        alu_lda: alu_res = src;
+        alu_cmp: alu_res = A - src;
+        alu_sbc: alu_res = A - src - (~P[flag_carry]);
+
+    endcase
+
+end
+
 endmodule
