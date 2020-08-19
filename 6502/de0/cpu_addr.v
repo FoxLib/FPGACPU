45e1d315666153ade0426441b25d35a0e4f9a0b0
FoxLib <hplus.foxtail@gmail.com>
2020-08-19 15:31:50 +0300

    Разделение

---
+++ 6502/de0/cpu_addr.v
@@ -0,0 +1,72 @@
+// Разбор опкода и установка метода адресации
+INI: begin
+
+    opcode  <= i_data;  // Записать опкод
+    implied <= 1'b0;    // Операнд не Implied по умолчанию
+    read_en <= 1'b1;    // Читать все из памяти (кроме STA)
+    cstate  <= EXE;     // По умолчанию следующий статус EXE
+
+    // Разобрать метод адресации
+    casex (i_data)
+
+        8'bxxx_0x0_x1: cstate <= NDX;
+        8'b1xx_000_x0: cstate <= EXE; // IMM
+        8'bxxx_100_x1: cstate <= NDY;
+        8'bxxx_110_x1: cstate <= ABY;
+        8'bxxx_0x1_xx: cstate <= ZP;
+        8'b001_000_00: cstate <= ABS;
+        8'b10x_101_1x: cstate <= ZPY;
+        8'bxxx_101_xx: cstate <= ZPX;
+        8'b10x_111_1x: cstate <= ABY;
+        8'bxxx_111_xx: cstate <= ABX;
+        8'bxxx_100_00: cstate <= REL;
+        default:       implied <= 1'b1; // ACC, IMP
+
+    endcase
+
+    // Все методы адресации разрешить читать из PPU, кроме STA
+    casex (i_data) 8'b100_xxx_01, 8'b100_xx1_x0: read_en = 1'b0; endcase
+
+end
+
+// Indirect, X ($00,X)
+// ---------------------------------------------------------------------
+NDX:   begin cstate <= cpunext; cursor <= i_data_x[7:0];   bus  <= 1'b1;    end
+NDX+1: begin cstate <= cpunext; cursor <= nextcursor[7:0]; tmp  <= i_data;  end
+NDX+2: begin cstate <= LAT;     cursor <= {i_data, tmp};   read <= read_en; end
+
+// Indirect, Y ($00),Y
+// ---------------------------------------------------------------------
+NDY:   begin cstate <= cpunext;   cursor <= i_data;               bus <= 1'b1; end
+NDY+1: begin cstate <= cpunext;   cursor <= nextcursor[7:0];      tmp <= i_data_y[7:0]; cout <= i_data_y[8]; end
+NDY+2: begin cstate <= lat_state; cursor <= {i_data + cout, tmp}; read <= read_en; end
+
+// ZP, ZPX, ZPY
+// ---------------------------------------------------------------------
+ZP:    begin cstate <= EXE; cursor <= i_data;        bus <= 1'b1; read <= read_en; end
+ZPX:   begin cstate <= LAT; cursor <= i_data_x[7:0]; bus <= 1'b1; read <= read_en; end
+ZPY:   begin cstate <= LAT; cursor <= i_data_y[7:0]; bus <= 1'b1; read <= read_en; end
+
+// Absolute
+// ---------------------------------------------------------------------
+ABS:   begin cstate <= cpunext; tmp <= i_data; pc <= pc + 1;  end
+ABS+1: begin // Либо отправляется на jmp (abs), либо используется как опкод
+
+    if (opcode == JMP_ABS)
+         begin cstate <= INI; pc     <= {i_data, tmp}; end
+    else begin cstate <= EXE; cursor <= {i_data, tmp}; bus <= 1'b1; read <= read_en; end
+
+end
+
+// Absolute,X
+// ---------------------------------------------------------------------
+ABX:   begin cstate <= cpunext;   tmp    <= i_data_x[7:0];        pc  <= pc + 1; cout <= i_data_x[8]; end
+ABX+1: begin cstate <= lat_state; cursor <= {i_data + cout, tmp}; bus <= 1'b1;   read <= read_en;     end
+
+// Absolute,Y
+// ---------------------------------------------------------------------
+ABY:   begin cstate <= cpunext;   tmp    <= i_data_y[7:0];        pc  <= pc + 1; cout <= i_data_y[8]; end
+ABY+1: begin cstate <= lat_state; cursor <= {i_data + cout, tmp}; bus <= 1'b1;   read <= read_en;     end
+
+// Задержка для симуляции тактов в 6502
+LAT:   cstate <= EXE;
