0808e8a6ed4141afed4936d32b27bb3a0f9b6f7b
FoxLib <hplus.foxtail@gmail.com>
2020-08-26 17:10:47 +0300

    Norton Commander будет перенесен

--- kr580/bios/mon.asm
+++ kr580/bios/mon.asm
@@ -26,39 +26,6 @@ start:
             ld      a, $07 + $08
             call    cls
 
-            ; Перерисовать панели
-            ld      hl, $8283
-            ld      d,  $80
-            call    bar
-
-            ld      b, 44
-M3:         ld      a, $81
-            rst     $08
-            ld      a, (cursor_xy)
-            add     a, 14
-            ld      (cursor_xy), a
-            ld      a, $81
-            rst     $08
-            djnz    M3
-
-            ld      hl, $8485
-            ld      d,  $80
-            call    bar
             jr      $
-; ---
-bar:        push    bc
-            ld      c, 2
-M2:         ld      a, h ; 0x82
-            rst     $08
-            ld      b, 14
-            ld      a, d ; 0x80
-M1:         rst     $08
-            djnz    M1
-            ld      a, l ; 0x83
-            rst     $08
-            dec     c
-            jr      nz, M2
-            pop     bc
-            ret
 
 ; ---------------------------------------------
