36387022cd195d17b52639c37588f122805a8133
FoxLib <hplus.foxtail@gmail.com>
2020-08-30 08:14:22 +0300

    Сделан почти что весь процессор, осталось много

--- bf/bf.v
+++ bf/bf.v
@@ -1,15 +1,28 @@
+/**
+ 0 >    i++;                перейти к следующей ячейке
+ 1 <    i--;                перейти к предыдущей ячейке
+ 2 +    arr[i]++;           увеличить значение в текущей ячейке на 1
+ 3 -    arr[i]--;           уменьшить значение в текущей ячейке на 1
+ 4 .    putchar(arr[i]);    напечатать значение из текущей ячейки
+ 5 ,    arr[i] = getchar(); ввести извне значение и сохранить в текущей ячейке
+ 6 [    while(arr[i]) {     если значение текущей ячейки ноль, перейти вперёд по тексту программы на ячейку, следующую за соответствующей ] (с учётом вложенности)
+ 7 ]    }                   если значение текущей ячейки не нуль, перейти назад по тексту программы на символ [ (с учётом вложенности)
+*/
+
 module bf(
 
     // Ответ
     input wire          clock,
-    input wire [7:0]    prg,         // Входящие данные из программы
-    input wire [7:0]    mem,         // Входящие данные из памяти
+    input wire [3:0]    i_prg,          // Входящие данные из программы
+    input wire [7:0]    i_din,          // Входящие данные из памяти
+    input wire [7:0]    keyb,           // Данные с клавиатуры (0-если ничего нет)
 
     // Запрос
     output reg [15:0]   pc,
     output reg [15:0]   cursor,
-    output reg [ 7:0]   out,
-    output reg          we
+    output reg [ 7:0]   out,            // Данные для записи
+    output reg          we,             // Сигнал записи в память
+    output reg          print           // Сигнал печати `out`
 );
 
 initial begin
@@ -18,6 +31,31 @@ initial begin
     cursor  = 0;
     out     = 0;
     we      = 0;
+    print   = 0;
+
+end
+
+reg  [2:0] tstate = 0;
+reg  [3:0] latch;
+wire [3:0] opcode = tstate ? latch : i_prg;
+
+always @(posedge clock) begin
+
+    we    <= 0;
+    print <= 0;
+
+    if (tstate == 0) begin latch <= i_prg; end
+
+    case (opcode)
+
+        /* > */ 0: begin pc <= pc + 1; cursor <= cursor + 1; end
+        /* < */ 1: begin pc <= pc + 1; cursor <= cursor - 1; end
+        /* + */ 2: begin pc <= pc + 1; out <= i_din + 1; we <= 1; end
+        /* - */ 3: begin pc <= pc + 1; out <= i_din - 1; we <= 1; end
+        /* . */ 4: begin pc <= pc + 1; out <= i_din; print <= 1; end
+        /* , */ 5: begin if (keyb) begin pc <= pc + 1; out <= keyb; we <= 1; end end
+
+    endcase
 
 end
 
--- bf/tb.v
+++ bf/tb.v
@@ -4,15 +4,17 @@ module tb;
 
 // ---------------------------------------------------------------------
 
-reg         clk;
-always #0.5 clk = ~clk;
+reg clock;
+reg clock25;
+always #0.5 clock   = ~clock;
+always #1.5 clock25 = ~clock25;
 
-initial begin clk = 1; #2000 $finish; end
+initial begin clock = 1; clock25 = 0; #2000 $finish; end
 initial begin $dumpfile("tb.vcd"); $dumpvars(0, tb); end
 
 // ---------------------------------------------------------------------
 
-reg  [ 7:0] prg;
+reg  [ 3:0] prg;
 reg  [ 7:0] mem;
 reg  [ 3:0] prgmem[65536];
 reg  [ 7:0] memory[65536];
@@ -20,16 +22,42 @@ wire [15:0] pc;                 // Адрес программы
 wire [15:0] cursor;             // Адрес памяти
 wire [ 7:0] out;                // Для записи
 wire        we;
+wire        print;              // Сигнал печати символа out
+reg  [ 7:0] keyb = 0;
 
 initial $readmemh("program.hex", prgmem, 16'h0000);
+integer i;
+
+initial for (i = 0; i < 1024; i++) memory[i] = 8'h00;
 
 /* Формируется логика чтения и записи в память */
-always @(posedge clk) begin
+always @(posedge clock) begin
 
     prg <= prgmem[ pc ];
     mem <= memory[ cursor ];
     if (we) memory[ cursor ] <= out;
 
 end
+
+always @(posedge clock25) begin
+
+    if (print) $display("Print: ", out);
+
+end
+
+// Модуль BF
+bf bfunit(
+
+    .clock      (clock25),
+    .i_prg      (prg),
+    .i_din      (mem),
+    .keyb       (keyb),
+
+    .pc         (pc),
+    .cursor     (cursor),
+    .out        (out),
+    .we         (we),
+    .print      (print)
+);
 
 endmodule
