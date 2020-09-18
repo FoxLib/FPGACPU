52199827e904f982605e230fd68a5bb6b58bd9af
FoxLib <hplus.foxtail@gmail.com>
2020-09-18 22:35:55 +0300

    Просто тестинг

--- avr64k/apps/template/main.cc
+++ avr64k/apps/template/main.cc
@@ -1,17 +1,21 @@
 #include <avrio.cc>
 
-int main() {
+void pset(word x, word y, byte cl) {
 
     heap(vm, 0xf000);
+    word t = y*320 + x;
+    bank((t >> 12) + 1);
+    vm[t & 0x0FFF] = cl;
+}
+
+int main() {
+
     videomode(2);
-    bank(1);
 
-    for (int j = 0; j < 16; j++) {
-
-        bank(1 + j);
-        for (int i = 0; i < 4096; i++)
-            vm[i] = i;
-    }
+    for (word y = 0; y < 200; y++)
+    for (word x = 0; x < 320; x++) {
+        pset(x, y, x);
+    };
 
     for(;;);
 }
