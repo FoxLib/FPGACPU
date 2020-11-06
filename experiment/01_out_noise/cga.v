eda524db8d4058c0c2be5e3f55c4b5c3713f6a21
FoxLib <hplus.foxtail@gmail.com>
2020-11-06 13:41:34 +0300

    Коммит...

--- experiment/01_out_noise/cga.v
+++ experiment/01_out_noise/cga.v
@@ -33,6 +33,17 @@ wire [10:0] X    = x - hz_back; // X=[0..639]
 wire [ 9:0] Y    = y - vt_back; // Y=[0..399]
 // ---------------------------------------------------------------------
 
+reg [31:0] rnd = 32'h00000001;
+
+// Следующий бит случайности
+wire next_bit = rnd[31] ^ rnd[30] ^ rnd[29] ^ rnd[27] ^ rnd[25] ^ rnd[0];
+
+// Генератор
+always @(posedge clock_25) rnd <= { next_bit, rnd[31:1] };
+
+wire [3:0] lv1 = rnd[3:0];
+wire [3:0] lv2 = (rnd[3:0]*3)/4;
+
 always @(posedge clock_25) begin
 
     // Кадровая развертка
@@ -43,7 +54,7 @@ always @(posedge clock_25) begin
     if (x >= hz_back && x < hz_visible + hz_back &&
         y >= vt_back && y < vt_visible + vt_back)
     begin
-         {R, G, B} <= 12'hFFF;
+         {R, G, B} <= Y[0] ? {lv1, lv1, lv1} : {lv2, lv2, lv2};
     end
     else {R, G, B} <= 12'h000;
 
