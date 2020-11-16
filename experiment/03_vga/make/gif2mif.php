6ff3706d8a18d525fd86fed455fe2e181ecc618b
FoxLib <hplus.foxtail@gmail.com>
2020-11-16 11:15:16 +0300

    Понт

---
+++ experiment/03_vga/make/gif2mif.php
@@ -0,0 +1,22 @@
+<?php
+
+// 16 bit
+$gif = imagecreatefromgif("main.gif");
+
+ob_start();
+echo "WIDTH=8;\nDEPTH=256;\nADDRESS_RADIX=HEX;\nDATA_RADIX=HEX;\n";
+echo "CONTENT BEGIN\n";
+
+$address = 0;
+for ($y = 0; $y < 480; $y++)
+for ($x = 0; $x < 640; $x += 2) {
+
+    $c1 = imagecolorat($gif, $x+0, $y);
+    $c0 = imagecolorat($gif, $x+1, $y);
+
+    $cl = $c1*16 + $c0;
+    echo sprintf("  %05x: %02x;\n", $address++, $cl);
+}
+
+echo "END\n";
+file_put_contents("vram.mif", ob_get_clean());
