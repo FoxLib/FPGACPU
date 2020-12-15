8d1a1e5b78a20756b5ecd88e1b0c3a43cd5f5c48
FoxLib <hplus.foxtail@gmail.com>
2020-12-15 13:08:33 +0300

    Закрепил календарь

--- 6502/core/de0/cgagen/make.php
+++ 6502/core/de0/cgagen/make.php
@@ -3,61 +3,61 @@
 $screen = [
 //             1         2         3         4         5         6         7
 //   01234567890123456789012345678901234567890123456789012345678901234567890123456789
-    "        Январь              Февраль             Март              Апрель        ", //  0
-    "Пн     4 11 18 25     |  1  8 15 22     |  1  8 15 22 29  |     5 12 19 26      ", //  1
-    "Вт     5 12 19 26     |  2  9 16 23     |  2  9 16 23 30  |     6 13 20 27      ", //  2
-    "Ср     6 13 20 27     |  3 10 17 24     |  3 10 17 24 31  |     7 14 21 28      ", //  3
-    "Чт     7 14 21 28     |  4 11 18 25     |  4 11 18 25     |  1  8 15 22 29      ", //  4
-    "Пт  1  8 15 22 39     |  5 12 19 26     |  5 12 19 26     |  2  9 16 23 30      ", //  5
-    "Сб  2  9 16 23 30     |  6 13 20 27     |  6 13 20 27     |  3 10 17 24         ", //  6
-    "Вс  3 10 17 24 31     |  7 14 21 28     |  7 14 21 28     |  4 11 18 25         ", //  7
-    "          Май                Июнь                Июль             Август        ", //  8
-    "Пн     3 10 17 24 31  |     7 14 21 28  |     5 12 19 26  |     2  9 16 23 30   ", //  9
-    "Вт     4 11 18 25     |  1  8 15 22 29  |     6 13 20 27  |     3 10 17 24 31   ", // 10
-    "Ср     5 12 19 26     |  2  9 16 23 30  |     7 14 21 28  |     4 11 18 25      ", // 11
-    "Чт     6 13 20 27     |  3 10 17 24     |  1  8 15 22 29  |     5 12 19 26      ", // 12
-    "Пт     7 14 21 28     |  4 11 18 25     |  2  9 16 23 30  |     6 13 20 27      ", // 13
-    "Сб  1  8 15 22 29     |  5 12 19 26     |  3 10 17 24 31  |     7 14 21 28      ", // 14
-    "Вс  2  9 16 23 30     |  6 13 20 27     |  4 11 18 25     |  1  8 15 22 29      ", // 15
-    "        Сентябрь            Октябрь            Ноябрь             Декабрь       ", // 16
-    "Пн     6 13 20 27     |     4 11 18 25  |  1  8 15 22 29  |     6 13 20 27      ", // 17
-    "Вт     7 14 21 28     |     5 12 19 26  |  2  9 16 23 30  |     7 14 21 28      ", // 18
-    "Ср  1  8 15 22 29     |     6 13 20 27  |  3 10 17 24     |  1  8 15 22 29      ", // 19
-    "Чт  2  9 16 23 30     |     7 14 21 28  |  4 11 18 25     |  2  9 16 23 30      ", // 20
-    "Пт  3 10 17 24        |  1  8 15 22 29  |  5 12 19 26     |  3 10 17 24 31      ", // 21
-    "Сб  4 11 18 25        |  2  9 16 23 30  |  6 13 20 27     |  4 11 18 25         ", // 22
-    "Вс  5 12 19 26        |  3 10 17 24 31  |  7 14 21 28     |  5 12 19 26         ", // 23
-    "                                                                                ", // 24
+    "         Январь              Февраль             Март              Апрель       ", //  0
+    " Пн     4 11 18 25     |  1  8 15 22     |  1  8 15 22 29  |     5 12 19 26     ", //  1
+    " Вт     5 12 19 26     |  2  9 16 23     |  2  9 16 23 30  |     6 13 20 27     ", //  2
+    " Ср     6 13 20 27     |  3 10 17 24     |  3 10 17 24 31  |     7 14 21 28     ", //  3
+    " Чт     7 14 21 28     |  4 11 18 25     |  4 11 18 25     |  1  8 15 22 29     ", //  4
+    " Пт  1  8 15 22 39     |  5 12 19 26     |  5 12 19 26     |  2  9 16 23 30     ", //  5
+    " Сб  2  9 16 23 30     |  6 13 20 27     |  6 13 20 27     |  3 10 17 24        ", //  6
+    " Вс  3 10 17 24 31     |  7 14 21 28     |  7 14 21 28     |  4 11 18 25        ", //  7
+    "           Май                Июнь                Июль             Август       ", //  8
+    " Пн     3 10 17 24 31  |     7 14 21 28  |     5 12 19 26  |     2  9 16 23 30  ", //  9
+    " Вт     4 11 18 25     |  1  8 15 22 29  |     6 13 20 27  |     3 10 17 24 31  ", // 10
+    " Ср     5 12 19 26     |  2  9 16 23 30  |     7 14 21 28  |     4 11 18 25     ", // 11
+    " Чт     6 13 20 27     |  3 10 17 24     |  1  8 15 22 29  |     5 12 19 26     ", // 12
+    " Пт     7 14 21 28     |  4 11 18 25     |  2  9 16 23 30  |     6 13 20 27     ", // 13
+    " Сб  1  8 15 22 29     |  5 12 19 26     |  3 10 17 24 31  |     7 14 21 28     ", // 14
+    " Вс  2  9 16 23 30     |  6 13 20 27     |  4 11 18 25     |  1  8 15 22 29     ", // 15
+    "         Сентябрь            Октябрь            Ноябрь             Декабрь      ", // 16
+    " Пн     6 13 20 27     |     4 11 18 25  |  1  8 15 22 29  |     6 13 20 27 ^__^", // 17
+    " Вт     7 14 21 28     |     5 12 19 26  |  2  9 16 23 30  |     7 14 21 28 (oo)", // 18
+    " Ср  1  8 15 22 29     |     6 13 20 27  |  3 10 17 24     |  1  8 15 22 29 (__)", // 19
+    " Чт  2  9 16 23 30     |     7 14 21 28  |  4 11 18 25     |  2  9 16 23 30   ||", // 20
+    " Пт  3 10 17 24        |  1  8 15 22 29  |  5 12 19 26     |  3 10 17 24 31   ||", // 21
+    " Сб  4 11 18 25        |  2  9 16 23 30  |  6 13 20 27     |  4 11 18 25        ", // 22
+    " Вс  5 12 19 26        |  3 10 17 24 31  |  7 14 21 28     |  5 12 19 26   2021 ", // 23
+    "                                                                          Корова", // 24
 ];
 
 $colors = [
 //             1         2         3         4         5         6         7
 //   01234567890123456789012345678901234567890123456789012345678901234567890123456789
-    "0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAAAAAA", //  0
-    "                       8                8                 8                     ", //  1
-    "                       8                8                 8                     ", //  2
-    "                       8                8                 8                     ", //  3
-    "                       8                8                 8                     ", //  4
-    "                       8                8                 8                     ", //  5
-    "CCCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCCC", //  6
-    "CCCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCCC", //  7
-    "AAAAAAAAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAAAAAA", //  8
-    "                       8                8                 8                     ", //  9
-    "                       8                8                 8                     ", // 10
-    "                       8                8                 8                     ", // 11
-    "                       8                8                 8                     ", // 12
-    "                       8                8                 8                     ", // 13
-    "CCCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCCC", // 14
-    "CCCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCCC", // 15
-    "AAAAAAAAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAAAAAA", // 16
-    "                       8                8                 8                     ", // 17
-    "                       8                8                 8                     ", // 18
-    "                       8                8                 8                     ", // 19
-    "                       8                8                 8                     ", // 20
-    "                       8                8                 8                     ", // 21
-    "CCCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCCC", // 22
-    "CCCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCCC", // 23
-    "                                                                                ", // 24
+    " 0AAAAAAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAAAAA", //  0
+    "    CC CC              8          CC     8    CC           8                    ", //  1
+    "    CC CC              8          CC     8                 8                    ", //  2
+    "    CC CC              8                 8                 8                    ", //  3
+    "    CC CC              8                 8                 8                    ", //  4
+    "    CC CC              8                 8                 8                    ", //  5
+    " CCCCCCCCCCCCCCCCCCCCCC8CCCCCCC  CCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCC", //  6
+    " CCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCC", //  7
+    " AAAAAAAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAAAAA", //  8
+    "       CC CC           8       CC        8                 8                    ", //  9
+    "                       8                 8                 8                    ", // 10
+    "                       8                 8                 8                    ", // 11
+    "                       8                 8                 8                    ", // 12
+    "                       8                 8                 8                    ", // 13
+    " CCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCC", // 14
+    " CCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCC", // 15
+    " AAAAAAAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAA8AAAAAAAAAAAAAAAAAAAA", // 16
+    "                       8                 8                 8                    ", // 17
+    "                       8                 8                 8                    ", // 18
+    "                       8                 8                 8                    ", // 19
+    "                       8                 8 CC              8                    ", // 20
+    "                       8                 8                 8             CC     ", // 21
+    " CCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCCCCC", // 22
+    " CCCCCCCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCCCC8CCCCCCCCCCCCCCAAAAAA", // 23
+    "                                                                          222222", // 24
 ];
 
 $font = file_get_contents("font.rom");
